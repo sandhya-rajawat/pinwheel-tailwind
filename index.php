7f5f28fa044c82efa76d7aa8dc479c2805b6da83
sandhyrajawat <sandhyarajawat1100@gmail.com>
2025-03-20 18:23:06 +0530

    work in progress

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+$view_blade = './index.blade.php';
+include './layouts/default.php';  
+
+?>
---
+++ layouts/default.php
@@ -0,0 +1,5 @@
+<?php
+include './partials/header.php';
+include $view_blade;
+include './partials/footer.php';
+?>
