65c1896bf009530371173e3a05c510f60fb319d2
sandhyrajawat <sandhyarajawat1100@gmail.com>
2025-03-21 13:15:47 +0530

    work in progress

--- partials/footer.php
+++ partials/footer.php
@@ -1,12 +1,5 @@
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
+
   
 <footer class="footer bg-theme-light/50">
   <div class="container">
@@ -138,5 +131,3 @@
 <!-- Main Script -->
 
 
-</body>
-</html>
--- partials/header.php
+++ partials/header.php
@@ -1,13 +1,4 @@
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  
 
 <header class="header">
 <nav class="navbar container">
@@ -119,6 +110,3 @@
   </div>
 </nav>
 </header>
-
-</body>
-</html>
