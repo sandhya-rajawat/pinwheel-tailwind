929ac1ff5b8e8be86e2c017ab306edf347f1e1e4
sandhyrajawat <sandhyarajawat1100@gmail.com>
2025-04-03 20:07:05 +0530

    blog-detail created

--- blog-detail.php
+++ blog-detail.php
@@ -1,26 +1,25 @@
 <?php
 include './function.php';
-$conn=$con = db_connect();
+$conn = db_connect();
+
 if (isset($_GET['id'])) {
-  $blog_id = $_GET['id'];
-  $stmt = $conn->prepare("SELECT * FROM blogs WHERE id = ?");
-  $stmt->bind_param("i", $blog_id);
-  $stmt->execute();
-  $result = $stmt->get_result();
+    $blog_id = $_GET['id'];
+    $stmt = $conn->prepare("SELECT * FROM blogs WHERE id = ?");
+    $stmt->bind_param("i", $blog_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-  if ($result->num_rows > 0) {
-      $blog = $result->fetch_assoc();
-      
-  } else {
-      session_flash('error', 'Blog Not Found!!');
+    if ($result->num_rows > 0) {
+        $blog = $result->fetch_assoc();
+    } else {
+        session_flash('error', 'Blog Not Found!!');
      
-  }
-
-  $stmt->close();
+    }
+    
+    $stmt->close();
 } else {
-  session_flash('error', 'Invalid Request');
-  header("Location: blog.php");
-  exit;
+    session_flash('error', 'Invalid Request');
+  
 }
 
 $conn->close();
@@ -28,6 +27,7 @@ $conn->close();
 $view_blade = './blog-detail.blade.php';
 include './layouts/default.php';
 ?>
+
 
 
 
